b1604b45a4612e82244c5e85a7c0e04f1ad697e3
verbess <shiminw@outlook.com>
2024-12-12 01:16:45 +0800

    feat: add YinyangPolarity enum

---
+++ src/Prototype/prototype/scripts/yinyang/YinyangPolarity.cs
@@ -0,0 +1,22 @@
+namespace Yinyang;
+
+/// <summary>
+/// Specifies the polarity of yinyang elements.
+/// </summary>
+public enum YinyangPolarity
+{
+    /// <summary>
+    /// The yin element.
+    /// </summary>
+    Yin = -1,
+
+    /// <summary>
+    /// The taiji element.
+    /// </summary>
+    Taiji = 0,
+
+    /// <summary>
+    /// The yang element.
+    /// </summary>
+    Yang = 1
+}
