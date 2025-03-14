b24351aee9867c969af011122d44114e3a72c1c9
verbess <shiminw@outlook.com>
2025-03-15 03:01:51 +0800

    refactor: update exception message

---
+++ src/Prototype/prototype/scripts/core/exceptions/Messages.cs
@@ -0,0 +1,15 @@
+namespace Yinyang;
+
+/// <summary>
+/// Serves as the resource class for exception error message constants.
+/// </summary>
+internal static class Messages
+{
+    #region Argument exceptions
+    /// <summary>
+    /// Thresholds must be ordered as: Yinji <= Yinchi <= 0 <= Yangchi <= Yangji.
+    /// </summary>
+    internal const string Argument_ThresholdsOutOfOrder =
+        @"Thresholds must be ordered as: Yinji <= Yinchi <= 0 <= Yangchi <= Yangji.";
+    #endregion
+}
