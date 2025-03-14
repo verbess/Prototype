7c622c6326d978ad556b59d6765ec1bb9fb01f55
verbess <shiminw@outlook.com>
2025-03-15 04:02:49 +0800

    refactor: update comments

---
+++ src/Prototype/prototype/scripts/core/yinyang/Polarity.cs
@@ -0,0 +1,31 @@
+namespace Yinyang;
+
+/// <summary>
+/// Specifies the polarity of yinyang elements.
+/// </summary>
+public enum Polarity
+{
+    /// <summary>
+    /// The yin polarity, when qi is negative.
+    /// </summary>
+    /// <remarks>
+    /// 炁，是中国古代的哲学概念，指产生和构成天地万物的原始能量，成为天地一切事物组成的基本元素。
+    /// </remarks>
+    Yin = -1,
+
+    /// <summary>
+    /// The taiji polarity, when qi is in the neutral balance.
+    /// </summary>
+    /// <remarks>
+    /// 炁，是中国古代的哲学概念，指产生和构成天地万物的原始能量，成为天地一切事物组成的基本元素。
+    /// </remarks>
+    Taiji = 0,
+
+    /// <summary>
+    /// The yang polarity, when qi is positive.
+    /// </summary>
+    /// <remarks>
+    /// 炁，是中国古代的哲学概念，指产生和构成天地万物的原始能量，成为天地一切事物组成的基本元素。
+    /// </remarks>
+    Yang = 1,
+}
