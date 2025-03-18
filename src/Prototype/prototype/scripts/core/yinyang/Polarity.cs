76319d2383c8cbac18d791ad825bc15a62daf5ba
verbess <shiminw@outlook.com>
2025-03-19 01:56:32 +0800

    refactor: move file and correct namespace casing

---
+++ src/Prototype/prototype/scripts/core/yinyang/Polarity.cs
@@ -0,0 +1,22 @@
+namespace Yinyang;
+
+/// <summary>
+/// Specifies the polarity of yinyang elements.
+/// </summary>
+public enum Polarity
+{
+    /// <summary>
+    /// The yin polarity, when qi is negative.
+    /// </summary>
+    Yin = -1,
+
+    /// <summary>
+    /// The taiji polarity, when qi is in the neutral balance.
+    /// </summary>
+    Taiji = 0,
+
+    /// <summary>
+    /// The yang polarity, when qi is positive.
+    /// </summary>
+    Yang = 1,
+}
