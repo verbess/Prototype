ae01f68632b5d3ee45ad5770500c79856ecdaf62
verbess <shiminw@outlook.com>
2025-04-10 01:31:25 +0800

    refactor: improve comments

--- src/Prototype/prototype/scripts/items/ItemData.cs
+++ src/Prototype/prototype/scripts/items/ItemData.cs
@@ -4,22 +4,19 @@ using Snake;
 namespace Yinyang.Items;
 
 /// <summary>
-///
+/// Represents immutable static data for an item within the game.
 /// </summary>
-/// <remarks>
-///
-/// </remarks>
 [GlobalClass]
 public sealed partial class ItemData : Resource, IKeyed
 {
     /// <summary>
-    ///
+    /// Initializes a new instance of this class with the specified properties.
     /// </summary>
-    /// <param name="key"></param>
-    /// <param name="name"></param>
-    /// <param name="description"></param>
-    /// <param name="types"></param>
-    /// <param name="price"></param>
+    /// <param name="key">The unique identifier of the item.</param>
+    /// <param name="name">The display name of the item.</param>
+    /// <param name="description">The brief description of the item.</param>
+    /// <param name="types">The type(s) of the item.</param>
+    /// <param name="price">The price of the item within the game's currency.</param>
     public ItemData(string key, string name, string description, ItemTypes types, int price)
     {
         ArgumentNullException.ThrowIfNull(key);
@@ -33,28 +30,26 @@ public sealed partial class ItemData : Resource, IKeyed
         Price = price;
     }
 
-    /// <summary>
-    ///
-    /// </summary>
+    /// <inheritdoc />
     public string Key { get; }
 
     /// <summary>
-    ///
+    /// Gets the display name of the item.
     /// </summary>
     public string Name { get; }
 
     /// <summary>
-    ///
+    /// Gets the brief description of the item.
     /// </summary>
     public string Description { get; }
 
     /// <summary>
-    ///
+    /// Gets the type(s) of the item.
     /// </summary>
     public ItemTypes Types { get; }
 
     /// <summary>
-    ///
+    /// Gets the price of the item within the game's currency.
     /// </summary>
     public int Price { get; }
 }
