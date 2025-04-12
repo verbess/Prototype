4f2a9b9c6c9f963e5ac6c69776a7b127a3ce5e7f
verbess <shiminw@outlook.com>
2025-04-13 03:55:47 +0800

    refactor: add YinyangData

--- src/Prototype/prototype/scripts/data/ItemData.cs
+++ src/Prototype/prototype/scripts/data/ItemData.cs
@@ -1,31 +1,37 @@
 using Snake;
+using Yinyang.Items;
 
-namespace Yinyang.Items;
+namespace Yinyang.Data;
 
 /// <summary>
 /// Represents immutable static data for an item within the game.
 /// </summary>
 public sealed record ItemData : IKeyed
 {
+    private readonly YinyangData _yinyangData;
+
     /// <summary>
     /// Initializes a new instance of this class with the specified properties.
     /// </summary>
-    /// <param name="key">The unique identifier of the item.</param>
+    /// <param name="key">The unique identifier of the data.</param>
     /// <param name="name">The display name of the item.</param>
     /// <param name="description">The brief description of the item.</param>
     /// <param name="types">The type(s) of the item.</param>
     /// <param name="price">The price of the item within the game's currency.</param>
-    public ItemData(string key, string name, string description, ItemTypes types, int price)
+    /// <param name="yinyangData"></param>
+    public ItemData(string key, string name, string description, ItemTypes types, int price, YinyangData yinyangData)
     {
         ArgumentNullException.ThrowIfNull(key);
         ArgumentNullException.ThrowIfNull(name);
         ArgumentNullException.ThrowIfNull(description);
+        ArgumentNullException.ThrowIfNull(yinyangData);
 
         Key = key;
         Name = name;
         Description = description;
         Types = types;
         Price = price;
+        _yinyangData = yinyangData;
     }
 
     /// <inheritdoc />
