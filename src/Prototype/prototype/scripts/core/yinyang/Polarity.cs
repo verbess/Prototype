faf961ef426680596b8ffed1b87baee0e3667da0
verbess <shiminw@outlook.com>
2025-04-15 17:00:59 +0800

    feat: add enum Polarity

---
+++ src/Prototype/prototype/scripts/core/yinyang/Polarity.cs
@@ -0,0 +1,22 @@
+namespace Yinyang;
+
+/// <summary>
+/// Represents the polarities of yinyang elements.
+/// </summary>
+public enum Polarity
+{
+    /// <summary>
+    /// The yin polarity, when the object's yinyang value is negative.
+    /// </summary>
+    Yin = -1,
+
+    /// <summary>
+    /// The taiji polarity, when the object's yinyang value is neutral.
+    /// </summary>
+    Taiji = 0,
+
+    /// <summary>
+    /// The yang polarity, when the object's yinyang value is positive.
+    /// </summary>
+    Yang = 1,
+}
