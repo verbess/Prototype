83078c415577d8ee883a0af207f7a643cbfa2a6c
王世民 <shiminw@outlook.com>
2025-04-25 00:08:32 +0800

    refactor: perf all (#38)
    * refactor: rm all
    
    * build: add gitignore
    
    * feat: add README
    
    * feat: add LICENSE
    
    * build: add vscode settings
    
    * feat: add auto tiles templates
    
    * build: add dll Snake
    
    * build: add editorconfig
    
    * feat: add godot project
    
    * build: add .csproj
    
    * feat: add assets
    
    * feat: add class Message
    
    * feat: add enum Polarity
    
    * feat: add interface IYinyang
    
    * feat: add interface IUnit
    
    * feat: add enum ItemTypes
    
    * feat: add interface IUsable
    
    * feat: add error message for Qi range validation
    
    * feat: add validation method for object's yinyang value
    
    * feat: add record YinyangData

--- src/Prototype/prototype/scripts/data/YinyangData.cs
+++ src/Prototype/prototype/scripts/data/YinyangData.cs
@@ -11,12 +11,13 @@ public sealed record YinyangData : IKeyed
     /// Initializes a new instance of this class with the specified properties.
     /// </summary>
     /// <param name="key">The unique identifier of the data.</param>
+    /// <param name="qi">The yinyang value of the object.</param>
     /// <param name="yinji">The yinyang value of the object's yinji threshold.</param>
     /// <param name="yinchi">The yinyang value of the object's yinchi threshold.</param>
     /// <param name="yangchi">The yinyang value of the object's yangchi threshold.</param>
     /// <param name="yangji">The yinyang value of the object's yangji threshold.</param>
-    /// <param name="initialPolarity">The initial polarity of the object.</param>
-    public YinyangData(string key, int yinji, int yinchi, int yangchi, int yangji, Polarity initialPolarity)
+    /// <param name="originalPolarity">The original polarity of the object.</param>
+    public YinyangData(string key, int qi, int yinji, int yinchi, int yangchi, int yangji, Polarity originalPolarity)
     {
         ArgumentNullException.ThrowIfNull(key);
         if (!IYinyang.AreThresholdsValid(yinji, yinchi, yangchi, yangji))
@@ -24,18 +25,29 @@ public sealed record YinyangData : IKeyed
             throw new ArgumentException(Messages.Argument_ThresholdsOutOfOrder);
         }
 
+        if (!IYinyang.IsQiValid(qi, yinji, yangji))
+        {
+            throw new ArgumentException(Messages.Argument_QiOutOfRange);
+        }
+
         Key = key;
+        Qi = qi;
         Yinji = yinji;
         Yinchi = yinchi;
         Yangchi = yangchi;
         Yangji = yangji;
-        InitialPolarity = initialPolarity;
+        OriginalPolarity = originalPolarity;
     }
 
     /// <summary>
     /// Gets the unique identifier of the data.
     /// </summary>
     public string Key { get; }
+
+    /// <summary>
+    /// Gets the yinyang value of the object.
+    /// </summary>
+    public int Qi { get; }
 
     /// <summary>
     /// Gets the yinyang value of the object's yinchi threshold.
@@ -58,7 +70,7 @@ public sealed record YinyangData : IKeyed
     public int Yangji { get; }
 
     /// <summary>
-    /// Gets the initial polarity of the object.
+    /// Gets the original polarity of the object.
     /// </summary>
-    public Polarity InitialPolarity { get; }
+    public Polarity OriginalPolarity { get; }
 }
