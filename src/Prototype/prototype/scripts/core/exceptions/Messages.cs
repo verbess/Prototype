e2922f29bcf07ec6ca90af2874f78eedc6e6fa26
verbess <shiminw@outlook.com>
2025-04-30 01:18:51 +0800

    refactor: update exception message formatting and add remarks

--- src/Prototype/prototype/scripts/core/exceptions/Messages.cs
+++ src/Prototype/prototype/scripts/core/exceptions/Messages.cs
@@ -9,12 +9,14 @@ internal static class Messages
     /// <summary>
     /// Thresholds must be ordered as: Yinji &lt;= Yinchi &lt;= 0 &lt;= Yangchi &lt;= Yangji.
     /// </summary>
+    /// <remarks>For details of the yinyang system, see <see cref="IYinyang"/>.</remarks>
     internal const string Argument_ThresholdsOutOfOrder =
-        @"Thresholds must be ordered as: Yinji <= Yinchi <= 0 <= Yangchi <= Yangji.";
+        "Thresholds must be ordered as: Yinji <= Yinchi <= 0 <= Yangchi <= Yangji.";
 
     /// <summary>
     /// Qi must be within the range: Yinji &lt;= Qi &lt;= Yangji.
     /// </summary>
-    internal const string Argument_QiOutOfRange = @"Qi must be within the range: Yinji <= Qi <= Yangji.";
+    /// <remarks>For details of the yinyang system, see <see cref="IYinyang"/>.</remarks>
+    internal const string Argument_QiOutOfRange = "Qi must be within the range: Yinji <= Qi <= Yangji.";
     #endregion
 }
