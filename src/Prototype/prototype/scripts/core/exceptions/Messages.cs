49f976edc23ee441b6c4bc0aae31eb9087190539
verbess <shiminw@outlook.com>
2025-04-30 18:19:13 +0800

    refactor: update remarks

--- src/Prototype/prototype/scripts/core/exceptions/Messages.cs
+++ src/Prototype/prototype/scripts/core/exceptions/Messages.cs
@@ -9,14 +9,14 @@ internal static class Messages
     /// <summary>
     /// Thresholds must be ordered as: Yinji &lt;= Yinchi &lt;= 0 &lt;= Yangchi &lt;= Yangji.
     /// </summary>
-    /// <remarks>For details of the yinyang system, see <see cref="IYinyang"/>.</remarks>
+    /// <remarks>For details of the yinyang system, read <see cref="IYinyang"/> interface.</remarks>
     internal const string Argument_ThresholdsOutOfOrder =
         "Thresholds must be ordered as: Yinji <= Yinchi <= 0 <= Yangchi <= Yangji.";
 
     /// <summary>
     /// Qi must be within the range: Yinji &lt;= Qi &lt;= Yangji.
     /// </summary>
-    /// <remarks>For details of the yinyang system, see <see cref="IYinyang"/>.</remarks>
+    /// <remarks>For details of the yinyang system, read <see cref="IYinyang"/> interface.</remarks>
     internal const string Argument_QiOutOfRange = "Qi must be within the range: Yinji <= Qi <= Yangji.";
     #endregion
 }
